fc9771bcf0f7216d2aa5b6f96d4de0621ba7dfa9
Olufemi Benjamin <benjaminolufemi16@gmail.com>
2023-04-17 13:19:31 -0500

    V2

--- src/features/car/carSlice.js
+++ src/features/car/carSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    cars: ["Model S", "Model 3", "Model X", "Model Y", "Accessories"]
+    cars: ["Model S", "Model 3", "Model X", "Model Y", "Solar Roof" "Solar Panels" "Powerwall"]
 }
 
 const carSlice = createSlice({
